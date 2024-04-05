f87209f5c666674c73a62df44cdc7fcacc087e1c
ShalvaMishvelidze <shalva.mishvelidze111@gmail.com>
2024-04-05 13:44:25 +0400

    added few more skills

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,91 +1,91 @@
-import React from 'react';
+import React from "react";
 import {
-	gpt3,
-	comfyJs,
-	comfyReact,
-	github,
-	githubSVG,
-	cocktails,
-	natours,
-	jobster,
-} from '../img';
+  gpt3,
+  comfyJs,
+  comfyReact,
+  github,
+  githubSVG,
+  cocktails,
+  natours,
+  jobster,
+} from "../img";
 
 const Projects = () => {
-	return (
-		<section id="projects">
-			<h2>Some Of My Projects</h2>
-			<div className="projects-container">
-				<a
-					target="_blank"
-					rel="noreferrer"
-					href="https://comfy-store-sm.netlify.app/"
-				>
-					<img src={comfyReact} alt="react e commerce app" />
-					<p>React Comfy Store</p>
-				</a>
+  return (
+    <section id="projects">
+      <h2>Some Of My Projects</h2>
+      <div className="projects-container">
+        <a
+          target="_blank"
+          rel="noreferrer"
+          href="https://comfy-store-sm.netlify.app/"
+        >
+          <img src={comfyReact} alt="react e commerce app" />
+          <p>React Comfy Store</p>
+        </a>
 
-				// <a
-				// 	target="_blank"
-				// 	rel="noreferrer"
-				// 	href="https://jobster-sm.netlify.app/"
-				// >
-				// 	<img src={jobster} alt="jobster" />
-				// 	<p>Job Tracker App</p>
-				// </a>
-				<a
-					target="_blank"
-					rel="noreferrer"
-					href="https://search-github-user-sm.netlify.app/"
-				>
-					<img src={github} alt="search github user app" />
-					<p>Search GitHub User</p>
-				</a>
-				<a
-					target="_blank"
-					rel="noreferrer"
-					href="https://comfy-store-js-sm.netlify.app/"
-				>
-					<img src={comfyJs} alt="e commerce app js" />
-					<p>JS Comfy Store</p>
-				</a>
-				<a
-					target="_blank"
-					rel="noreferrer"
-					href="https://gpt3-website-sm.netlify.app/"
-				>
-					<img src={gpt3} alt="gpt3 website clone" />
-					<p>GPT3 Website</p>
-				</a>
-				<a
-					target="_blank"
-					rel="noreferrer"
-					href="https://cocktails-sm.netlify.app/"
-				>
-					<img src={cocktails} alt="cocktails website js" />
-					<p>Cocktails Website</p>
-				</a>
-				<a
-					target="_blank"
-					rel="noreferrer"
-					href="https://natours-sm-html-css-only.netlify.app/"
-				>
-					<img src={natours} alt="cocktails website js" />
-					<p>Tours Website</p>
-				</a>
-			</div>
-			<h4>To view source code of my projects visit my Github page</h4>
-			<div className="github-container">
-				<a
-					target="_blank"
-					rel="noreferrer"
-					className="github-link"
-					href="https://github.com/shalvamishvelidze"
-				>
-					<img src={githubSVG} alt="github page link" />
-				</a>
-			</div>
-		</section>
-	);
+        <a
+          target="_blank"
+          rel="noreferrer"
+          href="https://jobster-sm.netlify.app/"
+        >
+          <img src={jobster} alt="jobster" />
+          <p>Job Tracker App</p>
+        </a>
+        <a
+          target="_blank"
+          rel="noreferrer"
+          href="https://search-github-user-sm.netlify.app/"
+        >
+          <img src={github} alt="search github user app" />
+          <p>Search GitHub User</p>
+        </a>
+        <a
+          target="_blank"
+          rel="noreferrer"
+          href="https://comfy-store-js-sm.netlify.app/"
+        >
+          <img src={comfyJs} alt="e commerce app js" />
+          <p>JS Comfy Store</p>
+        </a>
+        <a
+          target="_blank"
+          rel="noreferrer"
+          href="https://gpt3-website-sm.netlify.app/"
+        >
+          <img src={gpt3} alt="gpt3 website clone" />
+          <p>GPT3 Website</p>
+        </a>
+        <a
+          target="_blank"
+          rel="noreferrer"
+          href="https://cocktails-sm.netlify.app/"
+        >
+          <img src={cocktails} alt="cocktails website js" />
+          <p>Cocktails Website</p>
+        </a>
+        <a
+          target="_blank"
+          rel="noreferrer"
+          href="https://natours-sm-html-css-only.netlify.app/"
+        >
+          <img src={natours} alt="cocktails website js" />
+          <p>Tours Website</p>
+        </a>
+      </div>
+      <h4>To view source code of my projects visit my Github page</h4>
+      <div className="github-container">
+        <a
+          target="_blank"
+          rel="noreferrer"
+          className="github-link"
+          href="https://github.com/shalvamishvelidze"
+        >
+          <img src={githubSVG} alt="github page link" />
+        </a>
+      </div>
+    </section>
+  );
 };
 
 export default Projects;
--- src/img/index.js
+++ src/img/index.js
@@ -1,37 +1,41 @@
-import html from './html.svg';
-import figma from './figma.svg';
-import css from './css.svg';
-import sass from './sass.svg';
-import js from './javascript.svg';
-import ts from './typescript.svg';
-import react from './react.svg';
-import github from './search-github-user.png';
-import comfyJs from './comfy-store-js.png';
-import comfyReact from './comfy-store-react.png';
-import gpt3 from './gpt3.png';
-import githubSVG from './github.svg';
-import linkedIn from './linkedin.svg';
-import cocktails from './cocktails.png';
-import natours from './natours.png';
-import jobster from './jobster.png';
-import redux from './redux.svg';
+import html from "./html.svg";
+import figma from "./figma.svg";
+import css from "./css.svg";
+import sass from "./sass.svg";
+import js from "./javascript.svg";
+import ts from "./typescript.svg";
+import react from "./react.svg";
+import github from "./search-github-user.png";
+import comfyJs from "./comfy-store-js.png";
+import comfyReact from "./comfy-store-react.png";
+import gpt3 from "./gpt3.png";
+import githubSVG from "./github.svg";
+import linkedIn from "./linkedin.svg";
+import cocktails from "./cocktails.png";
+import natours from "./natours.png";
+import jobster from "./jobster.png";
+import redux from "./redux.svg";
+import vue from "./vue.svg";
+import next from "./next.svg";
 
 export {
-	figma,
-	sass,
-	ts,
-	html,
-	css,
-	js,
-	react,
-	github,
-	comfyJs,
-	comfyReact,
-	gpt3,
-	githubSVG,
-	linkedIn,
-	cocktails,
-	natours,
-	jobster,
-	redux,
+  figma,
+  sass,
+  ts,
+  html,
+  css,
+  js,
+  react,
+  github,
+  comfyJs,
+  comfyReact,
+  gpt3,
+  githubSVG,
+  linkedIn,
+  cocktails,
+  natours,
+  jobster,
+  redux,
+  vue,
+  next,
 };
